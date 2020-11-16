217c1ccab292d7924163a625d9811f21fb60c28c
Siddharth <sidd.kumaran@gmail.com>
2020-11-16 00:05:00 -0500

    sid

--- IMDBot/bot.py
+++ IMDBot/bot.py
@@ -221,7 +221,7 @@ def chooseMovie(list):
 # popular
 def popular():
     results = requests.get("https://api.themoviedb.org/3/trending/movie/week?api_key=77a3f22cc7407bb2b409d69b58fc32ab")
-    print(results)
+    print(results.json())
     return results
 
 # Main loop to keep the bot running.
