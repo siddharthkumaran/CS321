d02d6aac49ef078a70ce2a9f5e33cbe8aaaed19c
Siddharth <sidd.kumaran@gmail.com>
2020-11-23 14:38:12 -0500

    popular test case

--- IMDBot/test_getPopular.py
+++ IMDBot/test_getPopular.py
@@ -5,7 +5,7 @@ class Testbot(unittest.TestCase):
 
     def test_getRated(self):
         movie_id = bot.popular()
-        self.assertEqual(movie_id, 340102) #update this movie id by going to https://developers.themoviedb.org/3/trending/get-trending 
+        self.assertEqual(movie_id, 340102) #update this movie id by going to https://developers.themoviedb.org/3/trending/get-trending
 
 if __name__ == '__main__':
     unittest.main()
